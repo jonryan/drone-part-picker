10b300c378afd1cb9315c8f29f7c2fd15688b416
coreysnyder <coreysnyder@gmail.com>
2019-06-29 00:06:56 -0400

    You can now save new FCs with new page layout.

--- src/components/AddFlightController.jsx
+++ src/components/AddFlightController.jsx
@@ -23,45 +23,10 @@ const FC_MUTATION = gql`
 `
 
 class AddFlightController extends Component {
-  state = {
-    name: 'Test',
-    description: 'Test',
-    releaseDate: '11/5/2017',
-    uarts: 3,
-    weightInGrams: 7.5,
-    cpu: 'F7',
-    releaseDateUpdated: new Date(),
-    dimensions: '30.5x30.5',
-    holePattern: '30.5x30.5',
-    voltageInputMin: 5,
-    voltageInputMax: 25,
-    osd: true,
-
-  }
-
-  componentDidMount(){
-    console.log('this.state', this.state, this.props);
-    // If we're on the edit page for a FC
-    if(this.props.location.state && this.props.location.state.fc){
-      let fcId = this.props.location.state.fc;
-      // const data = store.readQuery({
-      //   query: FC_QUERY,
-      //   variables: { first, skip, orderBy }
-      // })
-    }
-  }
 
   render() {
 
-    const {
-      name, uarts, description, releaseDate, releaseDateUpdated,
-      dimensions, voltageInputMax, voltageInputMin, osd, holePattern,
-      cpu, weightInGrams,
-    } = this.state
-
-    let flightController = {...this.state}
-    flightController.releaseDate = this.state.releaseDateUpdated;
-    delete flightController.releaseDateUpdated;
+    const history = this.props.history
 
     return (
       <Container fluid>
@@ -73,16 +38,18 @@ class AddFlightController extends Component {
                 onSubmit={async (values, { setSubmitting, setErrors }) => {
                   console.log("values", values)
                   values.releaseDate = new Date(values.releaseDate + " 00:00:00")
-                  const { data } = await addFlightControllerEasy({ variables: { values } })
+                  const { data } = await addFlightControllerEasy({ variables: { flightController: values } })
 
+                  console.log('data', data)
                   setSubmitting(false)
-                  setErrors(transformGraphQLErrors(data.createArticle.errors))
-
-                  if (!_.isEmpty(data.createArticle.errors)) return
+                  // TODO: Get errors coming back from API in errors prop so they'll be picked up here
+                  // setErrors(transformGraphQLErrors(data.addFlightControllerEasy.errors))
+                  // if (!_.isEmpty(data.addFlightControllerEasy.errors)) return
 
                   // TODO: Figure out how to do this with the returne from my graphQL response
                   // const slug = _.get(data, 'createArticle.article.slug')
-                  // history.push(`/article/${slug}`)
+                  // history.push(`/flightcontroller/${slug}`)
+                  history.push(`/products/flight-controller/1`)
                 }}
               />
             </Page>
