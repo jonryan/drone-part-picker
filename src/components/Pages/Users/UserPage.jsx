c4a0fb1f0b75a361251f4f5a4f8aae9474f3bf77
Corey Snyder <corey.snyder@aver.io>
2019-07-27 00:36:52 -0400

    Wires up basic user edit form for changing user role and name, still more to go

--- src/components/Pages/Users/UserPage.jsx
+++ src/components/Pages/Users/UserPage.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Container, Table, Card, Row, Col, ListGroup, Form } from 'react-bootstrap';
-import {Query} from 'react-apollo';
+import React, {Fragment} from 'react';
+import {Container, Card, Row, Col, ListGroup, Form } from 'react-bootstrap';
+import {Mutation, Query} from 'react-apollo';
 import LoadingIndicatorCentered from './UsersPage.jsx';
 import {Link} from 'react-router-dom';
 import gql from 'graphql-tag';
 import Page from '../../Page';
 import moment from 'moment'
+import UserRoleDropdown from './UserRoleDropdown.jsx';
+import {Field, Formik} from 'formik';
 
 const GET_USER_QUERY = gql`
     query getUser($id: ID!){
@@ -29,6 +30,17 @@ const GET_USER_QUERY = gql`
             role
         }
     }
+`
+
+const EDIT_USER_MUTATION = gql`
+  mutation updateUser($user: UserEdit!){
+      updateUser(user: $user){
+          id
+          name
+          email
+          role
+      }
+  }
 `
 
 const BasicStaticFormGroup = ({title, value}) => {
@@ -105,90 +117,147 @@ const UserPage = props => {
             console.log('users', user);
 
             return (
-              <Container fluid className='mt-4'>
+              <Mutation
+                mutation={EDIT_USER_MUTATION}
+                update={(store, {data: {updateUser}}) => {
 
-                <BasicStaticFormGroup
-                  title='Name'
-                  value={user.name}
-                />
-                <BasicStaticFormGroup
-                  title='Email'
-                  value={user.email}
-                />
-                <BasicStaticFormGroup
-                  title='Role'
-                  value={(user.role) ? user.role : 'Not Set'}
-                />
+                  // const localStoreData = store.readQuery({
+                  //   query: MERCHANT_LIST_QUERY
+                  // })
+                  // let merchantList = localStoreData.merchantList.merchants
+                  // let matchedInStore = _.findWhere(merchantList, {id: updateMerchant.id})
+                  // matchedInStore = {...matchedInStore, ...updateMerchant}
+                  // // _.findWhere(merchantList, {id
+                  // // localStoreData.feed.links.unshift(post)
+                  // store.writeQuery({
+                  //   query: MERCHANT_LIST_QUERY,
+                  //   localStoreData
+                  // })
+                  console.log('Updated, reload?', data, updateUser)
+                }}
+              >
+                {updateUser => (
 
-                <Row>
-                  <Col md={6}>
-                    <BasicListCard
-                      title='Added Flight Controllers'
-                      arrayOfThings={user.flightControllers}
-                      lineItems={(fc) => (
-                        <React.Fragment>
-                          <Link
-                            to={{
-                              pathname: `/flight-controller/${fc.id}`
-                            }}>
-                            {fc.name}
-                          </Link> - {moment(fc.updatedAt).format('MMMM do YYYY')}
-                        </React.Fragment>
+                  <Container fluid className='mt-4'>
+                    <Formik
+                      initialValues={{
+                        name: user.name || '',
+                        role: user.role || null,
+                        disabled: user.disabled || undefined,
+                      }}
+                      onSubmit={async (values, { setSubmitting, setErrors }) => {
+                        console.log('onSubmit', data, values)
+                        values.id = data.getUser.id;
+                        const { data: mutationData } = await updateUser({
+                          variables: {user: values}
+                        })
+                        console.log('mutationData', data, mutationData)
+                        //
+                        setSubmitting(false)
+                        // history.push(`/merchants`)
+                      }}
+                    >
+                      {({values, isSubmitting, handleSubmit, setFieldValue, errors}) => (
+                        <Fragment>
+                          <form onSubmit={(formSubmitValues, anything) => {
+                            handleSubmit(formSubmitValues);
+                          }}>
+                            <fieldset disabled={isSubmitting}>
+                              <Form.Group controlId="name">
+                                <Form.Label>Name</Form.Label>
+                                <Field
+                                  name="name"
+                                  type="text"
+                                  autoComplete='off'
+                                  className="form-control"
+                                />
+                              </Form.Group>
+                              <BasicStaticFormGroup
+                                title='Email'
+                                value={user.email}
+                              />
+
+                              <UserRoleDropdown/>
+                              <Row>
+                                <Col md={6}>
+                                  <BasicListCard
+                                    title='Added Flight Controllers'
+                                    arrayOfThings={user.flightControllers}
+                                    lineItems={(fc) => (
+                                      <React.Fragment>
+                                        <Link
+                                          to={{
+                                            pathname: `/flight-controller/${fc.id}`
+                                          }}>
+                                          {fc.name}
+                                        </Link> - {moment(fc.updatedAt).format('MMMM do YYYY')}
+                                      </React.Fragment>
+                                    )}
+                                  />
+                                </Col>
+                                <Col md={6}>
+                                  <BasicListCard
+                                    title='Edited Flight Controllers'
+                                    arrayOfThings={user.editedFlightControllers}
+                                    lineItems={(fc) => (
+                                      <React.Fragment>
+                                        <Link
+                                          to={{
+                                            pathname: `/flight-controller/${fc.id}`
+                                          }}>
+                                          {fc.name}
+                                        </Link> - {moment(fc.updatedAt).format('MMMM do YYYY')}
+                                      </React.Fragment>
+                                    )}
+                                  />
+                                </Col>
+                              </Row>
+                              <Row>
+                                <Col md={6}>
+                                  <BasicListCard
+                                    title='Merchants Added'
+                                    arrayOfThings={user.addedMerchants}
+                                    lineItems={(merchant) => (
+                                      <React.Fragment>
+                                        <Link
+                                          to={{
+                                            pathname: `/edit-merchant/${merchant.id}`
+                                          }}>
+                                          {merchant.name}
+                                        </Link> - {moment(merchant.updatedAt).format('MMMM do YYYY')}
+                                      </React.Fragment>
+                                    )}
+                                  />
+                                </Col>
+                                <Col md={6}>
+                                  <BasicListCard
+                                    title='Merchants Edited'
+                                    arrayOfThings={user.editedMerchants}
+                                    lineItems={(merchant) => (
+                                      <React.Fragment>
+                                        <Link
+                                          to={{
+                                            pathname: `/edit-merchant/${merchant.id}`
+                                          }}>
+                                          {merchant.name}
+                                        </Link> - {moment(merchant.updatedAt).format('MMMM do YYYY')}
+                                      </React.Fragment>
+                                    )}
+                                  />
+                                </Col>
+                              </Row>
+
+                              <button type="submit" className="mt-3 btn btn-lg pull-xs-right btn-primary">
+                                Save User
+                              </button>
+                            </fieldset>
+                          </form>
+                        </Fragment>
                       )}
-                    />
-                  </Col>
-                  <Col md={6}>
-                    <BasicListCard
-                      title='Edited Flight Controllers'
-                      arrayOfThings={user.editedFlightControllers}
-                      lineItems={(fc) => (
-                        <React.Fragment>
-                          <Link
-                            to={{
-                              pathname: `/flight-controller/${fc.id}`
-                            }}>
-                            {fc.name}
-                          </Link> - {moment(fc.updatedAt).format('MMMM do YYYY')}
-                        </React.Fragment>
-                      )}
-                    />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={6}>
-                    <BasicListCard
-                      title='Merchants Added'
-                      arrayOfThings={user.addedMerchants}
-                      lineItems={(merchant) => (
-                        <React.Fragment>
-                          <Link
-                            to={{
-                              pathname: `/edit-merchant/${merchant.id}`
-                            }}>
-                            {merchant.name}
-                          </Link> - {moment(merchant.updatedAt).format('MMMM do YYYY')}
-                        </React.Fragment>
-                      )}
-                    />
-                  </Col>
-                  <Col md={6}>
-                    <BasicListCard
-                      title='Merchants Edited'
-                      arrayOfThings={user.editedMerchants}
-                      lineItems={(merchant) => (
-                        <React.Fragment>
-                          <Link
-                            to={{
-                              pathname: `/edit-merchant/${merchant.id}`
-                            }}>
-                            {merchant.name}
-                          </Link> - {moment(merchant.updatedAt).format('MMMM do YYYY')}
-                        </React.Fragment>
-                      )}
-                    />
-                  </Col>
-                </Row>
-              </Container>
+                    </Formik>
+                </Container>
+              )}
+              </Mutation>
             )
           }}
         </Query>
