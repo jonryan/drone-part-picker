8b2d11ae3675d7f4b845f0ce8c579ae98db9a017
Corey Snyder <corey.snyder@aver.io>
2019-07-26 23:10:42 -0400

    Adds new withUser component for attaching the current logged in user object to child components

--- src/components/WithUser.js
+++ src/components/WithUser.js
@@ -3,42 +3,28 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { Query } from 'react-apollo'
 
-const USER_FRAGMENT = gql`
-  fragment WithViewer on Viewer {
-    user {
-      id
-      username
-      email
-      image
-      bio
-    }
-  }
-`
-
 const GET_VIEWER = gql`
-  query Viewer {
-    viewer {
-      ...WithViewer
+    query {
+        me{
+            email
+            name
+            id
+        }
     }
-  },
-  ${USER_FRAGMENT}
 `
 
 const WithUser = ({ children }) => (
   <Query query={GET_VIEWER}>
     {({ loading, error, data }) => {
-      if (loading || error) return null
-      return children(data.viewer)
+      if (loading) return null
+      if (error) { return children(null)}
+      return children(data.me)
     }}
   </Query>
 )
 
 WithUser.propTypes = {
   children: PropTypes.func.isRequired
-}
-
-WithUser.fragments = {
-  viewer: USER_FRAGMENT
 }
 
 export default WithUser
